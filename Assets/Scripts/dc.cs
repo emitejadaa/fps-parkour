9f7d618c701053de4c04b650e2091bd247465978
emitejadaa <66174235+emitejadaa@users.noreply.github.com>
2025-05-23 09:05:07 -0300

    terminado

---
+++ Assets/Scripts/dc.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dc : MonoBehaviour
+{
+   void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("aaaaa");
+        Destroy(gameObject);
+    }
+}
